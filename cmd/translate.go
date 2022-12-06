7be36703f7a4c7194dbed2a4ec76694290aaadcd
batuhannoz <b.ozdmr23@gmail.com>
2022-12-06 12:42:38 +0300

    add db connection

--- cmd/translate.go
+++ cmd/translate.go
@@ -1,31 +1,23 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
 
+	"github.com/batuhannoz/lingo/data"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var (
-	rootCmd = &cobra.Command{
-		Use:   "lingo",
+	translateCmd = &cobra.Command{
+		Use:   "translate",
 		Short: "an app to learn and memorize new words",
 		Long:  `translate words into other languages, save as a list and memorize`,
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("root command")
+			data.CreateTable()
 		},
 	}
 )
 
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
 func init() {
-	rootCmd.AddCommand(translateCmd)
+
 }
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/batuhannoz/lingo/cmd"
+import (
+	"github.com/batuhannoz/lingo/cmd"
+	"github.com/batuhannoz/lingo/data"
+)
 
 func main() {
+	data.OpenDatabase()
 	cmd.Execute()
 }
